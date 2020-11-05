73e5f2257eee0c2462777236c2149a50c5e6f59e
raidentek <33987212+raidentek@users.noreply.github.com>
2020-11-04 21:14:01 -0500

    auth login

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,19 +5,23 @@ const userModel = require("../models/userModel");
 exports.createUser = async (req, res) => {
   try {
     const user = new userModel({
-        userName: req.body.userName,
-        email: req.body.email,
-        password: await bcrypt.hash(req.body.password, 10),
-        estado: req.body.estado,
-        role: req.body.role,
-    })
-    const existeEmail = await userModel.findOne({email: req.body.email})
+      userName: req.body.userName,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10),
+      passwordConfirm: req.body.passwordConfirm,
+      estado: req.body.estado,
+      role: req.body.role,
+    });
+
+    const existeEmail = await userModel.findOne({ email: req.body.email });
     if (existeEmail) {
-        res.json({ ok: false, message: 'Error: el email ya existe' });
+      res.json({ ok: false, message: "Error: el email ya existe" });
+    } else if (req.body.password !== req.body.passwordConfirm) {
+      res.json({ ok: false, message: "Error: los password no coinciden" });
     } else {
-        await user.save()
-        console.log(user);
-        res.json({ ok: true, message: 'Success: user created successfully' });
+      await user.save();
+      console.log(user);
+      res.json({ ok: true, message: "Success: user created successfully" });
     }
   } catch (error) {
     res.json({ ok: false, error });
